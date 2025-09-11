e3ce34eea76fe5b85b0e477d5be2882643d872fb
shestaya_liniya <pochta5550@gmail.com>
2025-09-11 16:50:22 +0200

    🔧 chore: (client) Vite - Add svg as components plugin and sass

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,10 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import solidSvg from 'vite-plugin-solid-svg'
 
 export default defineConfig({
-  plugins: [solidPlugin(), tailwindcss()],
+  plugins: [solidPlugin(), tailwindcss(), solidSvg()],
   server: {
     port: 1234
   },
