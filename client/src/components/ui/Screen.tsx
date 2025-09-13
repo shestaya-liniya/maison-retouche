db59e935b67915b4790aa80ab4e85ed82f83b4a1
shestaya_liniya <pochta5550@gmail.com>
2025-09-13 18:22:46 +0200

    ✨ feat: (client) UI - Screen

---
+++ client/src/components/ui/Screen.tsx
@@ -0,0 +1,97 @@
+import {
+  createEffect,
+  createSignal,
+  mergeProps,
+  on,
+  type ParentProps,
+  Show
+} from 'solid-js'
+import { Portal } from 'solid-js/web'
+
+import { getActions } from '@/global/actions'
+import { SLIDING_ANIMATION_MS } from '@/lib/common/const/animation'
+
+import Button from './Button'
+import styles from './Screen.module.scss'
+
+type OwnProps = ParentProps<{
+  isOpen: boolean
+  onClose: () => void
+  animation: 'slide' | 'fade'
+  onOpen?: () => void
+  onBeforeClose?: () => void
+  showBackButton?: boolean
+}>
+
+const Screen = (props: OwnProps) => {
+  props = mergeProps({ showBackButton: true }, props)
+
+  const { addBackButtonAction } = getActions()
+  const [isClosing, setIsClosing] = createSignal(false)
+
+  const handleClose = () => {
+    setIsClosing(true)
+    props.onBeforeClose?.()
+
+    setTimeout(() => {
+      props.onClose?.()
+      setIsClosing(false)
+    }, SLIDING_ANIMATION_MS)
+  }
+
+  createEffect(
+    on(
+      () => props.isOpen,
+      isOpen => {
+        if (isOpen) {
+          props.onOpen?.()
+
+          if (props.showBackButton) {
+            addBackButtonAction(handleClose)
+          }
+        } else {
+          if (!isClosing()) {
+            handleClose()
+          }
+        }
+      },
+      { defer: true }
+    )
+  )
+
+  return (
+    <Show when={props.isOpen || isClosing()}>
+      <Portal mount={document.querySelector('#screens-root') || undefined}>
+        <div
+          class={styles.wrapper}
+          classList={{
+            [styles.closing]: isClosing(),
+            [styles.backdrop]: props.animation === 'slide'
+          }}
+          onClick={props.onClose}
+        >
+          <div
+            class={styles.content}
+            classList={{
+              [styles.slide]: props.animation === 'slide',
+              [styles.fade]: props.animation === 'fade'
+            }}
+            onClick={e => e.stopPropagation()}
+          >
+            <Show when={import.meta.env.DEV}>
+              <Button
+                onClick={handleClose}
+                class="absolute top-0 right-0 opacity-20 z-20"
+              >
+                Back
+              </Button>
+            </Show>
+            {props.children}
+          </div>
+        </div>
+      </Portal>
+    </Show>
+  )
+}
+
+export default Screen
