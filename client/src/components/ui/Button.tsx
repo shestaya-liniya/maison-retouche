e57d52f025aa71fd9baf925f005b9933cabf5e78
shestaya_liniya <pochta5550@gmail.com>
2025-09-13 19:45:43 +0200

    🩹 fix: (client) UI - Fix disabled button reactivity

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -18,18 +18,23 @@ const Button = (props: OwnProps) => {
     'inline-flex overflow-hidden bg-button rounded-lg select-none px-4 py-2 font-medium',
     props.variant === 'transparent' && 'bg-transparent text-accent',
     props.isRound && 'rounded-full',
-    props.isDisabled && 'opacity-50',
     props.class
   )
 
   return (
-    <Tappable
-      onClick={props.onClick}
-      class={className}
-      isDisabled={props.isDisabled}
+    <div
+      classList={{
+        'opacity-50': props.isDisabled
+      }}
     >
-      <div>{props.children}</div>
-    </Tappable>
+      <Tappable
+        onClick={props.onClick}
+        class={className}
+        isDisabled={props.isDisabled}
+      >
+        <div>{props.children}</div>
+      </Tappable>
+    </div>
   )
 }
 
