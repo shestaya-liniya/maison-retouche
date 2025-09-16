a4e263daff56fd4975c38e4b7365f27791e19ffd
shestaya_liniya <pochta5550@gmail.com>
2025-09-16 20:54:45 +0200

    ✨ feat: (client) Api - Fetch user

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,6 +1,15 @@
+import { onMount } from 'solid-js'
+
 import PaycheckList from '@/components/PaycheckList'
+import { getActions } from '@/global/actions'
 
 const Main = () => {
+  const { fetchUser } = getActions()
+
+  onMount(() => {
+    fetchUser()
+  })
+
   return (
     <>
       <PaycheckList />
--- client/src/global/actions/api/user.ts
+++ client/src/global/actions/api/user.ts
@@ -1,9 +1,21 @@
+import { retrieveLaunchParams } from '@telegram-apps/sdk-solid'
+
 import { trpc } from '@/api/trpc'
+import { setGlobalState } from '@/global'
 
 export const userActions = {
-  fetchUser: (telegramId: number) => {
-    trpc.vendorController.getByTelegramId.query({
-      telegramId
+  fetchUser: async () => {
+    const lp = retrieveLaunchParams()
+    const user = lp.tgWebAppData?.user
+
+    if (!user) return
+
+    const apiUser = await trpc.vendorController.getByTelegramId.query({
+      telegramId: user.id
     })
+
+    console.log(apiUser)
+
+    setGlobalState('user', apiUser)
   }
 }
